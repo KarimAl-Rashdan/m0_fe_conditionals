488e74353c80631da3199f7a024156f866b02c9e
karimy <karim.alrashdan14@gmail.com>
2022-07-11 17:17:05 -0600

    Initial commit

---
+++ m0_fe_conditionals/comparisons.js
@@ -0,0 +1,17 @@
+var lovesToPlay = true;
+var lovesDogPark = false;
+var lovesTreats = true;
+var age = 1;
+
+// YOU DO:
+// Determine if the dog loves to play and loves treats
+console.log(lovesToPlay && lovesTreats)
+
+// Determine if the dog loves to play and loves the dog park
+console.log(lovesToPlay && lovesDogPark)
+
+// Determine if the dog loves to play or loves the dog park
+console.log(lovesToPlay || lovesDogPark)
+
+// Determine if the dog loves to play and is a puppy
+console.log(lovesToPlay && isAPuppy)
